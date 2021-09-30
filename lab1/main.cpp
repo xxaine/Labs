bf2368c358fcc3a40dcdd6db8c1095cc28d764e0
xxaine <90903362+xxaine@users.noreply.github.com>
2021-09-30 10:37:34 +0500

    Add files via upload

---
+++ lab1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  char x = 5;
+  char y = -10;
+  char z = x + y;
+  std::cout << "z= " << z << std::endl; 
+  
+  char h = 250;
+  char j = 40;
+  char k = h + j;
+  std::cout << "k= " << k << std::endl; 
+  return 0;
+}
